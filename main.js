3ca546c72ddd73859f6ded7240fcf18d47a7a4bc
kanishkg55 <80257854+kanishkg55@users.noreply.github.com>
2022-08-06 21:04:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+function preload() {
+	mario_gameover = loadSound("gameover.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_kick = loadSound("kick.wav");
+	mario_die = loadSound("mariodie.wav");
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+
+	instializeInSetup(mario);
+	
+	video = createCapture(VIDEO);
+	video.size(800,400);
+	video.parent('game_console');
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+
+}
+
+function modelLoaded() {
+	console.log('Model Loaded!');
+  }
+
+  function gotPoses(results)
+  {
+	if(results.length > 0)
+	{
+	  console.log(results);
+	  noseX = results[0].pose.nose.x;
+	  noseY = results[0].pose.nose.y;
+	}
+  }
+   
+function draw() {
+	game();
+}
+
+
+
+
+
+
